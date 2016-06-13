c94e826c0601ac8013210ae485bfc123388b753c
Emil Soman <emil.soman@gmail.com>
2016-06-13 09:22:07 +0530

    Gitignore mix.lock of apps inside the umbrella

--- apps/dockup/config/config.exs
+++ apps/dockup/config/config.exs
@@ -12,8 +12,7 @@ config :dockup,
   github_webhook_secret: nil,
   command_module: Dockup.ShellCommand,
   nginx_config_dir: "nginx_config_dir",
-  domain: "127.0.0.1.xip.io",
-  start_server: true
+  domain: "127.0.0.1.xip.io"
 #
 # And access this configuration in your application as:
 #
