ddf20da2fd3b177f6a5e4f3151afef822bc8433b
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-14 15:24:41 +0530

    Add reactjs deployments_container

---
+++ apps/dockup_ui/web/static/js/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import DeploymentForm from "web/static/js/deployment_form"
+ReactDOM.render(<DeploymentForm />, document.getElementById('deploy_container'));
