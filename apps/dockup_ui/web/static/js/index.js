0f2270f17f197ca8cf81c9798727f3ad32e4cd1b
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-15 14:13:38 +0530

    Organize react components in web/static/js/components in dockup_ui

--- apps/dockup_ui/web/static/js/index.js
+++ apps/dockup_ui/web/static/js/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import DeploymentForm from "web/static/js/deployment_form"
+import DeploymentForm from "web/static/js/components/deployment_form"
 ReactDOM.render(<DeploymentForm />, document.getElementById('deployments_container'));
