df03249ffd5b3332cc909dc6f1c9d511b36e66ef
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-16 13:12:28 +0530

    Import deployment.js and remove run function
    Import deployment.js in app.js. Remove run function from deployment.js

--- apps/dockup_ui/web/static/js/deployment.js
+++ apps/dockup_ui/web/static/js/deployment.js
@@ -1,6 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import DeploymentForm from "./components/deployment_form"
-export var run = function(){
-  ReactDOM.render(<DeploymentForm />, document.getElementById('deployments_container'));
-}
+ReactDOM.render(<DeploymentForm />, document.getElementById('deployments_container'));
