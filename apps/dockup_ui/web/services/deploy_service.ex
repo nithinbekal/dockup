2a32ea2ee837ee04dd1e80b2bd62e5069f7d7e13
Emil Soman <emil.soman@gmail.com>
2016-06-19 03:13:29 +0530

    NullCallback module when callback url is nil

--- apps/dockup_ui/web/services/deploy_service.ex
+++ apps/dockup_ui/web/services/deploy_service.ex
@@ -22,6 +22,11 @@ defmodule DockupUi.DeployService do
   end
 
   defp prepare_deployment_params(%{id: id, git_url: repo, branch: branch, callback_url: callback_url}) do
-    {id, repo, branch, {Dockup.Callbacks.Webhook, callback_url}}
+    callback = if is_nil(callback_url) do
+      Dockup.Callbacks.Null
+    else
+      Dockup.Callbacks.Webhook
+    end
+    {id, repo, branch, {callback, callback_url}}
   end
 end
