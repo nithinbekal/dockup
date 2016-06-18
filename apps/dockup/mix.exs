b0a12205c01b35de0b10313b9a2527a1db83ac7d
Emil Soman <emil.soman@gmail.com>
2016-06-19 03:03:28 +0530

    Add HTTPotion to dockup to fire callback urls

--- apps/dockup/mix.exs
+++ apps/dockup/mix.exs
@@ -12,7 +12,7 @@ defmodule Dockup.Mixfile do
 
   def application do
     [
-      applications: [:logger, :poison, :defmemo],
+      applications: [:logger, :poison, :defmemo, :httpotion],
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -23,7 +23,8 @@ defmodule Dockup.Mixfile do
   defp deps do
     [
       {:poison, "~>2.1.0"},
-      {:defmemo, "~> 0.1.1"}
+      {:defmemo, "~> 0.1.1"},
+      {:httpotion, "~> 3.0.0"}
     ]
   end
 end
