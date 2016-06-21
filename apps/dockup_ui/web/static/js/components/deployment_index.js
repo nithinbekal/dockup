079e793a6b171ccd5b15e3c8ac5f3d5a6f69d83c
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-21 13:56:12 +0530

    Handle form type on isGithub state

--- apps/dockup_ui/web/static/js/components/deployment_index.js
+++ apps/dockup_ui/web/static/js/components/deployment_index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import DeploymentForm from './deployment_form';
 import DeploymentList from './deployment_list';
+
 class DeploymentIndex extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deployments: []
+      deployments: [],
+      isGithub: true
     }
   }
 
@@ -14,11 +16,28 @@ class DeploymentIndex extends Component {
     deploymentsArray = deploymentsArray.concat(newDeployment)
     this.setState({deployments: deploymentsArray});
   }
+
+  handleClick(event) {
+    if(this.state.isGithub == true) {
+      this.setState({isGithub: false})
+    } else {
+      this.setState({isGithub: true});
+    }
+  }
   render() {
+    var value;
+    if(this.state.isGithub == true) {
+      value = "Not using github?"
+    } else {
+      value = "Using github?";
+    }
     return (
       <div>
-        <DeploymentForm newDeployment={this.addDeployment.bind(this)}/>
-        <DeploymentList deployments={this.state.deployments}/>
+        <div>
+          <a onClick={this.handleClick.bind(this)}>{value}</a>
+          <DeploymentForm newDeployment={this.addDeployment.bind(this)} isGithub={this.state.isGithub}/>
+          <DeploymentList deployments={this.state.deployments}/>
+        </div>
       </div>
     )
   }
