f43cb988f4f135a76e15a4040f44f01375554cc7
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-24 09:41:14 +0530

    Split url input into two components
    Split url input into GithubComponent and GenericComponent.

--- apps/dockup_ui/web/static/js/components/deployment_form.js
+++ apps/dockup_ui/web/static/js/components/deployment_form.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from "react-dom";
+import GithubComponent from './github_component';
+import GenericComponent from './generic_component';
 
 class DeploymentForm extends Component {
   constructor(props) {
@@ -17,14 +19,6 @@ class DeploymentForm extends Component {
     this.handleBranchChange = this.handleBranchChange.bind(this);
   }
 
-  handleUserNameChange(event) {
-    this.setState({username: event.target.value});
-  }
-
-  handleRepositoryChange(event) {
-    this.setState({repository: event.target.value});
-  }
-
   displayHelpText() {
     if (!this.state.username || !this.state.repository || !this.state.branch) {
       return "Please enter a github project and branch to deploy";
@@ -32,11 +26,11 @@ class DeploymentForm extends Component {
       return "Please enter a project url and branch to deploy";
     } else {
       return displayDeployMessage(this.props.isGithub, isAnyFieldEmpty(this.state.url,
-                                                                this.state.branch),
-                                                                this.state.url,
-                                                                this.state.branch,
-                                                                this.state.username,
-                                                                this.state.repository);
+                                                                       this.state.branch),
+                                                                       this.state.url,
+                                                                       this.state.branch,
+                                                                       this.state.username,
+                                                                       this.state.repository);
     }
   }
 
@@ -47,63 +41,44 @@ class DeploymentForm extends Component {
     this.props.newDeployment(newElement);
   }
 
-  handleUrlChange(event) {
-    this.setState({url: event.target.value});
+  handleUrlChange(url) {
+    this.setState({url: url});
   }
 
   handleBranchChange(event) {
     this.setState({branch: event.target.value});
   }
 
+  handleUserNameChange(username) {
+    this.setState({username: username});
+  }
+
+  handleRepositoryChange(repository) {
+    this.setState({repository: repository});
+  }
+
+
   render() {
-    let partial;
-    if(this.props.isGithub == true) {
-      partial = <div className="col-md-12">
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <b> https://github.com/ </b>
-                        </td>
-                        <td>
-                          <input name="username" onChange={this.handleUserNameChange} className="form-control"/>
-                        </td>
-                        <td>
-                          <b>/</b>
-                        </td>
-                        <td>
-                          <input name="repository" onChange={this.handleRepositoryChange} className="form-control"/>
-                        </td>
-                        <td>
-                          <b>.git</b>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-    } else {
-      partial = <div>
-                  Url :<input name="url" onChange={this.handleUrlChange} className="form-control"/>
-                </div>
-    }
     return (
       <div>
-        {partial}
-        <br />
-        <b>
-          Branch :
+      { this.props.isGithub ? <GithubComponent username={this.handleUserNameChange}
+        repository={this.handleRepositoryChange} /> : <GenericComponent
+        url={this.handleUrlChange}  />}
+      <br />
+      <b>
+      Branch :
         </b>
         <input name="branch" class="form-control" onChange={this.handleBranchChange} className="form-control" />
         <div>{this.displayHelpText()}</div>
         <button name="deploy" onClick={this.handleClick} disabled={(!this.state.username || !this.state.repository)} className="btn btn-primary">Deploy</button>
-      </div>
+        </div>
     )
   }
 }
-let isAnyFieldEmpty = function(field1, field2) {
+const isAnyFieldEmpty = function(field1, field2) {
   return (field1 || field2);
 }
-let displayDeployMessage = function(isGithub, isAnyFieldEmpty, url, branch, username, repository) {
+const displayDeployMessage = function(isGithub, isAnyFieldEmpty, url, branch, username, repository) {
   if ((isGithub == false) && isAnyFieldEmpty) {
     return `Click the Deploy button to deploy ${url} with branch: ${branch}`
   } else {
