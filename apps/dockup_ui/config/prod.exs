f005ee22f7891b94cda851bf2663ebbc05d2d39f
Emil Soman <emil.soman@gmail.com>
2016-07-24 00:07:37 +0530

    Read dockup host and port from env

--- apps/dockup_ui/config/prod.exs
+++ apps/dockup_ui/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :dockup_ui, DockupUi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: (System.get_env("DOCKUP_DOMAIN") || "example.com"), port: (System.get_env("DOCKUP_PORT") || 80)],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
