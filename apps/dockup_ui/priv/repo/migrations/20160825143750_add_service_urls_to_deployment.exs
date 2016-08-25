5c2065bbb4af56ce987138150b0f4857cfdf9339
Nithin Bekal <nithinbekal@gmail.com>
2016-08-26 02:39:02 +0530

    Add new field in create_deployment migration
    Since we don't have a production instance and we're cool with wiping DBs
    everywhere at this point, let's add this straight into the
    create_deployment migration.

--- apps/dockup_ui/priv/repo/migrations/20160825143750_add_service_urls_to_deployment.exs
+++
@@ -1,9 +0,0 @@
-defmodule DockupUi.Repo.Migrations.AddServiceUrlsToDeployment do
-  use Ecto.Migration
-
-  def change do
-    alter table(:deployments) do
-      add :service_urls, :json
-    end
-  end
-end
